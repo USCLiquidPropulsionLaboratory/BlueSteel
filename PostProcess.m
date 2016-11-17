d37e60ed319fefbbe73f5e31512769856c3aacc0
Marcel223 <mlarivie@usc.edu>
2016-11-16 20:15:54 -0800

    Update PostProcess.m

--- PostProcess.m
+++ PostProcess.m
@@ -17,6 +17,7 @@
 % Date Modified: 11/16/2016
 
 %Clear variables used in this script
+
 clc
 
 %Import comma separated value data exactly as is into table variable T
@@ -81,7 +82,6 @@ MaxBurnValTime = zeros(width(Tdata),1);
 MaxBurnVal = zeros(width(Tdata),1);
 AvgVal = zeros(width(Tdata),1);
 AvgBurnVal = zeros(width(Tdata),1);
-
 %Find Max & Avg Values foreach column of data, write to arrays
 jCol=2;
 for jCol=2:1:width(Tdata)
@@ -114,7 +114,7 @@ delete(findall(gcf,'Tag','Event 1 Textbox'))
 delete(findall(gcf,'Tag','Event 2 Textbox'))
 delete(findall(gcf,'Tag','Event 3 Textbox'))
 %Manually plot Pressure Transducer 1 data
-subplot(4,2,1)
+h1=subplot(4,2,1)
 plot(time,Tdata.(2),NomTime,NomGraph(2,:),'-.k', MaxValTime(2),MaxVal(2),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(2)),...
     ['Nominal: ' num2str(Nominal(2)) ' psi'],['P1 max: ' num2str(MaxVal(2)) ' psi'])
@@ -142,7 +142,6 @@ annotation('textbox',...
     'FitBoxToText','on',...
     'HorizontalAlignment','center',...
     'tag', 'Event 3 Textbox')
-
 %Create counter for looping through event information
 i = DataEndIdx+2;
 %Plot events on graph
@@ -150,9 +149,8 @@ i = DataEndIdx+2;
 for i = i:1:length(time)
 line(time(i)*[1 1], get(gca,'YLim'),'Color','r','LineStyle',':')
 end
-
 %Manually plot Pressure Transducer 2 data
-subplot(4,2,3)
+h2=subplot(4,2,3)
 plot(time,Tdata.(3),NomTime,NomGraph(3,:),'-.k',MaxValTime(3),MaxVal(3),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(3)),...
     ['Nominal: ' num2str(Nominal(3)) ' psi'],['P2 max: ' num2str(MaxVal(3)) ' psi'])
@@ -167,10 +165,9 @@ i = DataEndIdx+2;
 for i = i:length(time)
 line(time(i)*[1 1], get(gca,'YLim'),'Color','r','LineStyle',':');
 end
-
 %
 %Manually plot Pressure Transducer 3 data
-subplot(4,2,5)
+h3=subplot(4,2,5)
 plot(time,Tdata.(4),NomTime,NomGraph(4,:),'-.k',MaxValTime(4),MaxVal(4),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(4)),...
     ['Nominal: ' num2str(Nominal(4)) ' psi'],['P3 max: ' num2str(MaxVal(4)) ' psi'])
@@ -185,10 +182,9 @@ i = DataEndIdx+2;
 for i = i:1:length(time)
 line(time(i)*[1 1], get(gca,'YLim'),'Color','r','LineStyle',':')
 end
-
 %
 %Manually plot Pressure Transducer 4 data
-subplot(4,2,7)
+h4=subplot(4,2,7)
 plot(time,Tdata.(5),NomTime,NomGraph(5,:),'-.k',MaxValTime(5),MaxVal(5),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(5)),...
     ['Nominal: ' num2str(Nominal(5)) ' psi'],['P4 max: ' num2str(MaxVal(5)) ' psi'])
@@ -205,7 +201,7 @@ line(time(i)*[1 1], get(gca,'YLim'),'Color','r','LineStyle',':')
 end
 %
 %Manually plot Pressure Transducer 5 data
-subplot(4,2,2)
+h5=subplot(4,2,2)
 plot(time,Tdata.(6),NomTime,NomGraph(6,:),'-.k',MaxValTime(6),MaxVal(6),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(6)),...
     ['Nominal: ' num2str(Nominal(6)) ' psi'],['P5 max: ' num2str(MaxVal(6)) ' psi'])
@@ -222,7 +218,7 @@ line(time(i)*[1 1], get(gca,'YLim'),'Color','r','LineStyle',':')
 end
 %
 %Manually plot Pressure Transducer 6 data
-subplot(4,2,4)
+h6=subplot(4,2,4)
 plot(time,Tdata.(7),NomTime,NomGraph(7,:),'-.k',MaxValTime(7),MaxVal(7),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(7)),...
     ['Nominal: ' num2str(Nominal(7)) ' psi'],['P6 max: ' num2str(MaxVal(7)) ' psi'])
@@ -239,7 +235,7 @@ line(time(i)*[1 1], get(gca,'YLim'),'Color','r','LineStyle',':')
 end
 %
 %Manually plot Pressure Transducer 7 data
-subplot(4,2,6)
+h7=subplot(4,2,6)
 plot(time,Tdata.(8),NomTime,NomGraph(8,:),'-.k',MaxValTime(8),MaxVal(8),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(8)),...
     ['Nominal: ' num2str(Nominal(8)) ' psi'],['P7 max: ' num2str(MaxVal(8)) ' psi'])
@@ -276,7 +272,7 @@ line(time(i)*[1 1], get(gca,'YLim'),'Color','r','LineStyle',':')
 end
 %
 %Manually plot Force data
-subplot(4,2,8)
+h8=subplot(4,2,8)
 plot(time,Tdata.(9),NomTime,NomGraph(9,:),'-.k',MaxValTime(9),MaxVal(9),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(9)),...
     ['Nominal: ' num2str(Nominal(9)) ' N'],['Force max: ' num2str(MaxVal(9)) ' [N]'])
@@ -295,7 +291,7 @@ end
 %Create new full-window figure for graphs during Burn only
 figure('units','normalized','outerposition',[0 0 1 1])
 %Manually plot Pressure Transducer 1 data
-subplot(4,2,1)
+g1=subplot(4,2,1)
 plot(timeBurn,Tburn.(2),NomTime,NomGraph(2,:),'-.k',MaxBurnValTime(2),MaxBurnVal(2),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(2)),...
     ['Nominal: ' num2str(Nominal(2)) ' psi'],['P1 max: ' num2str(MaxBurnVal(2)) ' psi'])
@@ -306,7 +302,7 @@ ylabel('Pressure [psi]')
 title('Pressure Transducer 1 during Burn Time')
 %
 %Manually plot Pressure Transducer 2 data
-subplot(4,2,3)
+g2=subplot(4,2,3)
 plot(timeBurn,Tburn.(3),NomTime,NomGraph(3,:),'-.k',MaxBurnValTime(3),MaxBurnVal(3),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(3)),...
     ['Nominal: ' num2str(Nominal(3)) ' psi'],['P2 max: ' num2str(MaxBurnVal(3)) ' psi'])
@@ -316,7 +312,7 @@ ylabel('Pressure [psi]')
 title('Pressure Transducer 2 during Burn Time')
 %
 %Manually plot Pressure Transducer 3 data
-subplot(4,2,5)
+g3=subplot(4,2,5)
 plot(timeBurn,Tburn.(4),NomTime,NomGraph(4,:),'-.k',MaxBurnValTime(4),MaxBurnVal(4),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(4)),...
     ['Nominal: ' num2str(Nominal(4)) ' psi'],['P3 max: ' num2str(MaxBurnVal(4)) ' psi'])
@@ -326,7 +322,7 @@ ylabel('Pressure [psi]')
 title('Pressure Transducer 3 during Burn Time')
 %
 %Manually plot Pressure Transducer 4 data
-subplot(4,2,7)
+g4=subplot(4,2,7)
 plot(timeBurn,Tburn.(5),NomTime,NomGraph(5,:),'-.k',MaxBurnValTime(5),MaxBurnVal(5),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(5)),...
     ['Nominal: ' num2str(Nominal(5)) ' psi'],['P4 max: ' num2str(MaxBurnVal(5)) ' psi'])
@@ -336,7 +332,7 @@ ylabel('Pressure [psi]')
 title('Pressure Transducer 4 during Burn Time')
 %
 %Manually plot Pressure Transducer 5 data
-subplot(4,2,2)
+g5=subplot(4,2,2)
 plot(timeBurn,Tburn.(6),NomTime,NomGraph(6,:),'-.k',MaxBurnValTime(6),MaxBurnVal(6),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(6)),...
     ['Nominal: ' num2str(Nominal(6)) ' psi'],['P5 max: ' num2str(MaxBurnVal(6)) ' psi'])
@@ -346,7 +342,7 @@ ylabel('Pressure [psi]')
 title('Pressure Transducer 5 during Burn Time')
 %
 %Manually plot Pressure Transducer 6 data
-subplot(4,2,4)
+g6=subplot(4,2,4)
 plot(timeBurn,Tburn.(7), NomTime,NomGraph(7,:),'-.k',MaxBurnValTime(7),MaxBurnVal(7),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(7)),...
     ['Nominal: ' num2str(Nominal(7)) ' psi'],['P6 max: ' num2str(MaxBurnVal(7)) ' psi'])
@@ -356,7 +352,7 @@ ylabel('Pressure [psi]')
 title('Pressure Transducer 6  during Burn Time')
 %
 %Manually plot Pressure Transducer 7 data
-subplot(4,2,6)
+g7=subplot(4,2,6)
 plot(timeBurn,Tburn.(8), NomTime,NomGraph(8,:),'-.k',MaxBurnValTime(8),MaxBurnVal(8),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(8)),...
     ['Nominal: ' num2str(Nominal(8)) ' psi'],['P7 max: ' num2str(MaxBurnVal(8)) ' psi'])
@@ -367,7 +363,7 @@ ylabel('Pressure [psi]')
 title('Pressure Transducer 7 during Burn Time')
 %
 %Manually plot Force data
-subplot(4,2,8)
+g8=subplot(4,2,8)
 plot(timeBurn,Tburn.(9), NomTime,NomGraph(9,:),'-.k',MaxBurnValTime(9),MaxBurnVal(9),'dr','linewidth',0.75)
 legend(char(T.Properties.VariableNames(9)),...
     ['Nominal: ' num2str(Nominal(9)) ' N'],['Force max: ' num2str(MaxBurnVal(9)) ' N'])
@@ -375,6 +371,7 @@ xlim([timeBurn(1) time(end)])
 xlabel('Time [s]')
 ylabel('Force [N]')
 title('Thrust during Burn Time')
+
 set(h1, 'ButtonDownFcn', @mycallbackfcn)
 set(h2, 'ButtonDownFcn', @mycallbackfcn2)
 set(h3, 'ButtonDownFcn', @mycallbackfcn3)
@@ -383,3 +380,12 @@ set(h5, 'ButtonDownFcn', @mycallbackfcn5)
 set(h6, 'ButtonDownFcn', @mycallbackfcn6)
 set(h7, 'ButtonDownFcn', @mycallbackfcn7)
 set(h8, 'ButtonDownFcn', @mycallbackfcn8)
+
+set(g1, 'ButtonDownFcn', @mycallbackfcng1)
+set(g2, 'ButtonDownFcn', @mycallbackfcng2)
+set(g3, 'ButtonDownFcn', @mycallbackfcng3)
+set(g4, 'ButtonDownFcn', @mycallbackfcng4)
+set(g5, 'ButtonDownFcn', @mycallbackfcng5)
+set(g6, 'ButtonDownFcn', @mycallbackfcng6)
+set(g7, 'ButtonDownFcn', @mycallbackfcng7)
+set(g8, 'ButtonDownFcn', @mycallbackfcng8)
